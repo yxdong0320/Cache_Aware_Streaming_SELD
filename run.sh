0dd57d08308018623c3d37247b6cdf74ebe1ff50
yxdong0320 <anonymous@mail.ustc.edu.cn>
2025-07-31 00:24:05 +0800

    250731

--- run.sh
+++ run.sh
@@ -4,20 +4,20 @@ export CUDA_VISIBLE_DEVICES=3
 #         -c Test_Dual_Cache_APC_RC_24h_Chunk[100,49]_L8EM256_Fustep10_APC_NS_to_S \
 #         -m results/Dual_Cache_APC_RC_24h_Chunk[100,49]_L8EM256_Fustep10_APC/checkpoints/checkpoint_epoch165_step90090.pth
 
-# nohup python finetune_A_cache_chunk_limited_TS_hidstate_att_loss.py \
-#         -c Finetune_T0.40_Cache_RC_24h_Chunk[100,4]_8ConformerLayer_CMAPC_loss \
-#         > train_logs/Finetune_T0.40_Cache_RC_24h_Chunk[100,4]_8ConformerLayer_CMAPC_loss.log &
+nohup python finetune_A_cache_chunk_limited_TS_hidstate_att_loss.py \
+        -c Finetune_T0.40_Cache_RC_24h_Chunk[100,4]_8ConformerLayer_att_loss_last_layer2 \
+        > train_logs/Finetune_T0.40_Cache_RC_24h_Chunk[100,4]_8ConformerLayer_att_loss_last_layer3.log &
 
 
 # python finetune_A_cache_chunk_limited_TS_hidstate_att_loss.py \
 #         -c Finetune_T[100,49]_Cache_RC_24h_Chunk[100,4]_8ConformerLayer_ts_cache_t
-nohup python train_A_cache_chunk_limited_TS_hidstate_att_srd_loss.py \
-        -c Train_T0.40_Cache_RC_24h_Chunk[100,4]_8ConformerLayer_CMAPC_loss \
-        > train_logs/Train_T0.40_Cache_RC_24h_Chunk[100,4]_8ConformerLayer_CMAPC_loss.log &
+# nohup python train_A_cache_chunk_limited_TS_hidstate_att_srd_loss.py \
+#         -c Train_T0.40_Cache_RC_24h_Chunk[100,4]_8ConformerLayer_CMAPC_loss \
+#         > train_logs/Train_T0.40_Cache_RC_24h_Chunk[100,4]_8ConformerLayer_CMAPC_loss.log &
 
 # nohup python train_A_dual_mode_cache_CrossModalAPC.py \
-#         -c Dual_Cache_APC_RC_24h_Chunk[100,49]_L8EM256_Fustep10_APC_MSELoss \
-#         > train_logs/Dual_Cache_APC_RC_24h_Chunk[100,49]_L8EM256_Fustep10_APC_MSELoss_3.log &
+#         -c Dual_Cache_APC_RC_24h_Chunk[100,49]_L8EM256_Fustep10_CMAPC \
+#         > train_logs/Dual_Cache_APC_RC_24h_Chunk[100,49]_L8EM256_Fustep10_CMAPC.log &
 
 # python finetune_A_cache_chunk_limited_TS_hidstate_att_loss.py \
 #         -c Finetune_T0.40_Cache_RC_24h_Chunk[100,4]_8ConformerLayer_hiddenstate_loss \
